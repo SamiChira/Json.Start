46bd3c3b81e6340162ae9dd4c03c8d7664f097f7
SamiChira <99469882+SamiChira@users.noreply.github.com>
2022-02-14 19:50:44 +0200

    More refactor....

--- Json.Validator/JsonString.cs
+++ Json.Validator/JsonString.cs
@@ -11,24 +11,34 @@ namespace Json
                 return false;
             }
 
-            return QuotesChecks(input) &&
-                ContainsUnnescapedControlCharacter(input);
+            return Quoted(input) &&
+                   IsEmptyDoubleQuoted(input);
         }
 
-        static bool QuotesChecks(string input)
+        static bool Quoted(string input)
         {
             return input.StartsWith('"') && input.EndsWith('"') &&
-                   IsEmptyDoubleQuoted(input);
+                   AlwaysStartsWithQuotes(input);
+        }
+
+        static bool AlwaysStartsWithQuotes(string input)
+        {
+            const int numberTwo = 2;
+            int quotesCounter = 0;
+            foreach (var item in input)
+            {
+                if (item == '"')
+                {
+                    quotesCounter++;
+                }
+            }
+
+            return quotesCounter % numberTwo == 0 && Quoted(input);
         }
 
         static bool IsEmptyDoubleQuoted(string input)
         {
-            return QuotesChecks(input);
-        }
-
-        static bool ContainsUnnescapedControlCharacter(string input)
-        {
-            return false;
+            return Quoted(input);
         }
     }
 }
