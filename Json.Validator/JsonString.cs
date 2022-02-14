e0a1eef94b2a5f1e6f06727fd1ce3f31a39d2afa
SamiChira <99469882+SamiChira@users.noreply.github.com>
2022-02-14 12:15:10 +0200

    Always starts with quotes passed.

--- Json.Validator/JsonString.cs
+++ Json.Validator/JsonString.cs
@@ -6,12 +6,28 @@ namespace Json
     {
         public static bool IsJsonString(string input)
         {
-            return Quoted(input);
+            return Quoted(input) &&
+                AlwaysStartsWithQuotes(input);
         }
 
         static bool Quoted(string input)
         {
             return input.StartsWith('"') && input.EndsWith('"');
+        }
+
+        static bool AlwaysStartsWithQuotes(string input)
+        {
+            const int numberTwo = 2;
+            int quotesCounter = 0;
+            foreach (var item in input)
+            {
+                if (item == '"')
+                {
+                    quotesCounter++;
+                }
+            }
+
+            return quotesCounter % numberTwo == 0 && Quoted(input);
         }
     }
 }
