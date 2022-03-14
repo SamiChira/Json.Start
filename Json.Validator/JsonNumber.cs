28a0901fd6302ccd32024c40266ba222d30f8426
SamiChira <99469882+SamiChira@users.noreply.github.com>
2022-03-14 10:22:36 +0200

    Refractored IsDigit(). Added StartsWithZero(). DoesNotStartWithZero() passed

--- Json.Validator/JsonNumber.cs
+++ Json.Validator/JsonNumber.cs
@@ -11,20 +11,25 @@ namespace Json
 
         static bool IsDigit(string input)
         {
-            if (string.IsNullOrEmpty(input) || input[0] == '0')
+            if (string.IsNullOrEmpty(input))
             {
                 return false;
             }
 
             foreach (var item in input)
             {
-                if (item > '9' || item < '0')
+                if (StartsWithZero(input) || (item < 48 || item > 57))
                 {
                     return false;
                 }
             }
 
             return true;
+        }
+
+        private static bool StartsWithZero(string input)
+        {
+            return input.Length > 1 && input[0] == '0';
         }
     }
 }
