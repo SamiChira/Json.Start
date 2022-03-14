10ea6636c8c544afa86e5270c7f7527c5fe56076
SamiChira <99469882+SamiChira@users.noreply.github.com>
2022-03-14 09:33:29 +0200

    Start from scratch JsonNumber.

--- Json.Validator/JsonNumber.cs
+++ Json.Validator/JsonNumber.cs
@@ -6,24 +6,7 @@ namespace Json
     {
         public static bool IsJsonNumber(string input)
         {
-            return HasContent(input) &&
-                   !ValidStartAndEndFormat(input) &&
-                   HasValidContent(input);
-        }
-
-        static bool HasContent(string input)
-        {
-            return !string.IsNullOrEmpty(input);
-        }
-
-        static bool HasValidContent(string input)
-        {
-            return double.TryParse(input, out double resultFractional);
-        }
-
-        static bool ValidStartAndEndFormat(string input)
-        {
-            return input.StartsWith('0') && input.Length > 1 && !input.Contains('.') || input.EndsWith('.');
+            return true;
         }
     }
 }
