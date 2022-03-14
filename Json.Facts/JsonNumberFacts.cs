7ab46e8bba4458d9799fee7fecf8ee2c2050478f
SamiChira <99469882+SamiChira@users.noreply.github.com>
2022-03-14 09:41:05 +0200

    IsNotAnEmptyString() passed

--- Json.Facts/JsonNumberFacts.cs
+++ Json.Facts/JsonNumberFacts.cs
@@ -36,7 +36,7 @@ namespace Json.Facts
             Assert.False(IsJsonNumber(null));
         }
 
-        [Fact(Skip = "Remove after implement")]
+        [Fact]
         public void IsNotAnEmptyString()
         {
             Assert.False(IsJsonNumber(string.Empty));
