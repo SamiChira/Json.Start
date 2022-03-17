ef4cba2952fd3ab49a0bc426e51a363b32731f88
SamiChira <99469882+SamiChira@users.noreply.github.com>
2022-03-17 15:59:19 +0200

    Refractored IsValidInteger()

--- Json.Validator/JsonNumber.cs
+++ Json.Validator/JsonNumber.cs
@@ -37,7 +37,12 @@ namespace Json
 
         private static bool IsValidInteger(string integer)
         {
-            return integer.StartsWith('-') ? IsDigits(integer[1 ..]) : IsDigits(integer) && !StartsWithZero(integer);
+            if (integer.StartsWith('-'))
+            {
+                integer = integer[1..];
+            }
+
+            return IsDigits(integer) && !StartsWithZero(integer);
         }
 
         private static string Exponent(string input, int exponentIndex)
