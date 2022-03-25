595fd8081e4fcdf564184f6eb3db4695930318c8
SamiChira <99469882+SamiChira@users.noreply.github.com>
2022-03-25 13:17:17 +0200

    Added Elements To Team()

--- Json.Validator/Team.cs
+++ Json.Validator/Team.cs
@@ -1,10 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace Json
+namespace SoccerRanking
 {
-    internal class Team
+    public class Team
     {
+        private readonly string name;
+        private int points;
+
+        public Team(string name, int points)
+        {
+            this.name = name;
+            this.points = points;
+        }
+
+        public string TeamStats()
+        {
+            return name + " : " + points.ToString();
+        }
+
+        public void TeamStatsUpdate(int awardedPoints)
+        {
+            points += awardedPoints;
+        }
+
+        public bool EqualsTeamName(string teamName)
+        {
+            return name.Equals(teamName);
+        }
     }
 }
