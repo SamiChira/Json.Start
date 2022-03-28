946144b6fc4955d73a466ccb4e65d3ea663dc92a
SamiChira <99469882+SamiChira@users.noreply.github.com>
2022-03-28 18:15:29 +0300

    Added Ranking, and Team.cs

---
+++ Ranking/Team.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+
+namespace Ranking
+{
+    public class Team
+    {
+        private string name;
+        private int points;
+
+        public Team(string name, int points)
+        {
+            this.name = name;
+            this.points = points;
+        }
+
+        public string TeamStats()
+        {
+            return name + " : " + points.ToString();
+        }
+
+        public void TeamStatsUpdate(int awardedPoints)
+        {
+            points += awardedPoints;
+        }
+
+        public bool EqualsTeamName(string teamName)
+        {
+            return name.Equals(teamName);
+        }
+    }
+}
