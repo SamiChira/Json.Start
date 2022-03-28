c6c2ae9e1809e9c703c78db98ceb3b043897a0e5
SamiChira <99469882+SamiChira@users.noreply.github.com>
2022-03-28 18:25:14 +0300

    Added Ranking.Facts & TeamFacts.cs

---
+++ TeamFacts/TeamFacts.cs
@@ -0,0 +1,42 @@
+using Xunit;
+
+namespace Ranking.Facts
+{
+    public class TeamFacts
+    {
+        [Fact]
+        public void TeamStatsReturnCorrectValueWhenOneIsEntered()
+        {
+            Team steaua = new Team("steaua", 1);
+
+            Assert.True(steaua.TeamStats() == "steaua : 1");
+        }
+
+        [Fact]
+        public void WhenTeamStatsUpdateIsCalledShouldChangePointsValueIfValueIsGreaterThanZero()
+        {
+            Team steaua = new Team("steaua", 0);
+            int awardedPoints = 3;
+
+            steaua.TeamStatsUpdate(awardedPoints);
+
+            Assert.True(steaua.TeamStats() == "steaua : 3");
+        }
+
+        [Fact]
+        public void WhenEqualsTeamNameIsCalledShouldReturnTrueIfTeamEnteredHasTheSameNameAsTheSearchedName()
+        {
+            Team steaua = new Team("steaua", 0);
+
+            Assert.True(steaua.EqualsTeamName("steaua"));
+        }
+
+        [Fact]
+        public void WhenEqualsTeamNameIsCalledShouldReturnFalseIfTeamEnteredHasNotTheSameNameAsTheSearchedName()
+        {
+            Team steaua = new Team("steaua", 0);
+
+            Assert.False(steaua.EqualsTeamName("cfr"));
+        }
+    }
+}
