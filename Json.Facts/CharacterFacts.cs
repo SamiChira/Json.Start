5851d25e52620a1425b846546df22cfda3abb01d
SamiChira <99469882+SamiChira@users.noreply.github.com>
2022-04-20 18:20:15 +0300

    Added Character.cs and CharacterFacts.cs

---
+++ Json.Facts/CharacterFacts.cs
@@ -0,0 +1,75 @@
+﻿using Xunit;
+
+
+namespace Json.Facts
+{
+    public class CharacterFacts
+    {
+        [Fact]
+        public void WhenMatchIsCalledAndInputIsNullShoulReturnFalse()
+        {
+            Character character = new Character('a');
+
+            Assert.False(character.Match(null));
+        }
+
+        [Fact]
+        public void WhenMatchIsCalledAndInputDoesNotStartWithGivenPatternShouldReturnFalse()
+        {
+            Character character = new Character('a');
+
+            Assert.False(character.Match("bac"));
+            Assert.False(character.Match("b"));
+        }
+
+        [Fact]
+        public void WhenMatchIsCalledAndInputStartsWithGivenPatternShouldReturnTrue()
+        {
+            Character character = new Character('a');
+
+            Assert.True(character.Match("a"));
+            Assert.True(character.Match("abc"));
+        }
+
+        [Fact]
+        public void WhenOverloadMatchIsCalledAndInputIsNotInGivenLimitsShouldReturnFalse()
+        {
+            Character digitOne = new Character('1');
+            Character letterA = new Character('a');
+
+            Assert.False(digitOne.Match('2', '9'));
+            Assert.False(letterA.Match('b', 'z'));
+        }
+
+        [Fact]
+        public void WhenOverloadMatchIsCalledAndInputIsInGivenLimitsShouldReturnTrue()
+        {
+            Character digitOne = new Character('1');
+            Character letterA = new Character('a');
+
+            Assert.True(digitOne.Match('1', '9'));
+            Assert.True(letterA.Match('a', 'z'));
+        }
+
+        [Fact]
+        public void WhenMatchUnicodeIsCalledAndInputIsNullShouldReturnFalse()
+        {
+            Character character = new Character('a');
+
+            Assert.False(character.MatchUnicode(null));
+        }
+
+        [Fact]
+        public void WhenMatchUnicodeIsCalledAndInputedUnicodeIsValidUnicodeReturnsTrue()
+        {
+            
+        }
+
+    /*
+    [Fact]
+    public void ()
+    {
+
+    }*/
+}
+}
---
+++ Json.Validator/Character.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Json
+{
+    public class Character
+    {
+        readonly char pattern;
+
+        public Character(char pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public bool Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return false;
+            }
+
+            return text[0] == pattern;
+        }
+
+        public bool Match(char start, char end) => start <= pattern && pattern <= end;
+
+        public bool MatchUnicode(string text)
+        {
+            const int unicodeValidLength = 6;
+            const int unicodeHexValidLength = 4;
+
+            if (string.IsNullOrEmpty(text) || text.Length < unicodeValidLength)
+            {
+                return false;
+            }
+
+            if (text[0] != '\\' || text[1] != 'u')
+            {
+                return false;
+            }
+
+            for (int i = 0; i < unicodeHexValidLength; i++)
+            {
+                Character character = new Character(text[i]);
+                if (text[text.IndexOf('u') ..].Length < unicodeHexValidLength
+                    && !character.Match('a', 'f')
+                    && !character.Match('A', 'F')
+                    && !character.Match('0', '9'))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
