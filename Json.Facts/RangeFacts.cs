6ca1701ffdf11ae3a3429e88cf9edaca745c6885
SamiChira <99469882+SamiChira@users.noreply.github.com>
2022-04-21 12:32:35 +0300

    Added Range.cs & RangeFacts.cs

---
+++ Json.Facts/RangeFacts.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+
+namespace Json.Facts
+{
+    public class RangeFacts
+    {
+        [Fact]
+        public void WhenMatchIsCalledAndTextEnteredIsNullShouldReturnFalse()
+        {
+            var range = new Range('a', 'z');
+
+            Assert.False(range.Match(null));
+        }
+
+        [Fact]
+        public void WhenMatchIsCalledAndTextEnteredIsInGivenRangeShouldReturnTrue()
+        {
+            var range = new Range('a', 'f');
+
+            Assert.True(range.Match("abc"));
+            Assert.True(range.Match("fab"));
+            Assert.True(range.Match("bcd"));
+        }
+
+        [Fact]
+        public void WhenMatchIsCalledAndTextEnteredContainsCharacterOutsideGivenLimitsReturnsFalse()
+        {
+            var range = new Range('a', 'f');
+
+            Assert.False(range.Match("1ab"));
+            Assert.False(range.Match("abg"));
+        }
+    }
+}
---
+++ Json.Validator/Range.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Json
+{
+    public class Range
+    {
+        readonly char start;
+        readonly char end;
+
+        public Range(char start, char end)
+        {
+            this.start = start;
+            this.end = end;
+        }
+
+        public bool Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return false;
+            }
+
+            foreach (var item in text)
+            {
+                if (item < start || item > end)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
