4f57673261e72bebde8214cbb757ccb5e8083faf
SamiChira <99469882+SamiChira@users.noreply.github.com>
2022-05-03 09:35:18 +0300

    Added ChoiceFacts.cs

---
+++ Json.Facts/ChoiceFacts.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+
+namespace Json.Facts
+{
+    public class ChoiceFacts
+    {
+        [Fact]
+        public void WhenMatchIsCalledAndTextToAnalyzeIsNullOrEmptyShouldReturnFalse()
+        {
+            var digit = new Choice(
+                new Character('0'),
+                new Range('0', '9')
+                );
+
+            Assert.False(digit.Match(null));
+            Assert.False(digit.Match(""));
+        }
+    }
+}
