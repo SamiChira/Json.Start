4fd024084f4155ad61927177ee4cb259b72bb014
SamiChira <99469882+SamiChira@users.noreply.github.com>
2022-05-12 09:35:22 +0300

    REfratored CHoice and ChoiceFacts

--- Json.Validator/Choice.cs
+++ Json.Validator/Choice.cs
@@ -16,36 +16,17 @@
                 return false;
             }
 
-            string digits = GetDigits(text, '0', '9');
-            string letters = GetLetters(text.ToLower(), 'a', 'z');
-            byte matchesCounter = 0;
+            bool patternsMatches = false;
 
             for (int i = 0; i < patterns.Length; i++)
             {
-                if (patterns[i].Match(text) || (patterns[i].Match(digits) || patterns[i].Match(letters)))
+                if (patterns[i].Match(text))
                 {
-                    matchesCounter++;
+                    patternsMatches = patterns[i].Match(text) || patternsMatches;
                 }
             }
 
-            return digits.Length > 0 && letters.Length > 0 ? matchesCounter > 1 : matchesCounter > 0;
-        }
-
-        string GetLetters(string text, char start, char end) => GetDigits(text, start, end);
-
-        string GetDigits(string text, char start, char end)
-        {
-            string digits = "";
-
-            foreach (var item in text)
-            {
-                if (item >= start && item <= end)
-                {
-                    digits += item;
-                }
-            }
-
-            return digits;
+            return patternsMatches;
         }
     }
 }
