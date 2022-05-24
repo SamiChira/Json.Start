92ce4810ce7f4b486b3bab3436cb0646dcc903f4
SamiChira <99469882+SamiChira@users.noreply.github.com>
2022-05-24 15:31:33 +0300

    Added Optional.cs & OptionalFacts.cs

---
+++ Json.Facts/OptionalFacts.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+
+namespace Json.Facts
+{
+    public class OptionalFacts
+    {
+        [Fact]
+        public void WhenMatchIsCalledAndTextEnteredIsNullOrEmptyReturnsTrue()
+        {
+            var optional = new Optional(new Character('a'));
+
+            Assert.True(optional.Match("").Succes() && optional.Match("").RemainingText() == "");
+            Assert.True(optional.Match(null).Succes() && optional.Match(null).RemainingText() == null);
+        }
+
+        [Fact]
+        public void WhenMatchIsCalledAndGivenMatchesEnteredTextReturnsTrueAndRemainingText()
+        {
+            var a = new Optional(new Character('a'));
+            var sign = new Optional(new Character('-'));
+
+            Assert.True(a.Match("abc").Succes() && a.Match("abc").RemainingText() == "bc");
+            Assert.True(a.Match("aabc").Succes() && a.Match("aabc").RemainingText() == "abc");
+            Assert.True(sign.Match("-123").Succes() && sign.Match("-123").RemainingText() == "123");
+        }
+
+        [Fact]
+        public void WhenMatchIsCalledAndGivenPatternsDoesNotMatchTextReturnsTrueAndTextEntered()
+        {
+            var a = new Optional(new Character('a'));
+            var sign = new Optional(new Character('-'));
+
+            Assert.True(a.Match("bc").Succes() && a.Match("bc").RemainingText() == "bc");
+            Assert.True(sign.Match("123").Succes() && sign.Match("123").RemainingText() == "123");
+        }
+    }
+}
---
+++ Json.Validator/Optional.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Json
+{
+    public class Optional
+    {
+        readonly IPattern pattern;
+
+        public Optional(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            return new Match(pattern.Match(text).RemainingText(), true);
+        }
+    }
+}
