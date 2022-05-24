73424cb112818f7effb7c061b6f90e5ea4c5299c
SamiChira <99469882+SamiChira@users.noreply.github.com>
2022-05-24 10:10:39 +0300

    Added Many.cs && ManyFacts,cs

---
+++ Json.Facts/ManyFacts.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+
+namespace Json.Facts
+{
+    public class ManyFacts
+    {
+        [Fact]
+        public void WhenMatchIsCalledAndTextEnteredIsNullOrEmptyReturnsTrue()
+        {
+            var many = new Many(new Character('a'));
+
+            Assert.True(many.Match("").Succes());
+            Assert.True(many.Match(null).Succes());
+        }
+
+        [Fact]
+        public void WhenMatchIsCalledAndTextEnteredIsNotNullOrEmptyIfPatternMatchesTextOrNotReturnsTrueAndRemainingText()
+        {
+            var a = new Many(new Character('a'));
+            var digits = new Many(new Range('0', '9'));
+
+            Assert.True(a.Match("abc").Succes() && a.Match("abc").RemainingText() == "bc");
+            Assert.True(a.Match("aaaabc").Succes() && a.Match("aaaabc").RemainingText() == "bc");
+            Assert.True(a.Match("bc").Succes() && a.Match("bc").RemainingText() == "bc");
+            Assert.True(digits.Match("123").Succes() && digits.Match("123").RemainingText() == "");
+            Assert.True(digits.Match("12345ab123").Succes() && digits.Match("12345ab123").RemainingText() == "ab123");
+            Assert.True(digits.Match("ab").Succes() && digits.Match("ab").RemainingText() == "ab");
+
+
+        }
+    }
+}
---
+++ Json.Validator/Many.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Json
+{
+    public class Many : IPattern
+    {
+        readonly IPattern pattern;
+
+        public Many(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            IMatch match = new Match(text, true);
+            if (string.IsNullOrEmpty(text))
+            {
+                return match;
+            }
+
+            while (pattern.Match(match.RemainingText()).Succes())
+            {
+                match = pattern.Match(match.RemainingText());
+            }
+
+            return match;
+        }
+    }
+}
