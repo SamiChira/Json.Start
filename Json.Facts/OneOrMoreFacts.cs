693a4a677a6212095bc64080c92c6d8f0fa034a1
SamiChira <99469882+SamiChira@users.noreply.github.com>
2022-06-06 09:26:02 +0300

    Added OneOrMore.cs & OneOreMoreFacts.cs

---
+++ Json.Facts/OneOrMoreFacts.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+
+namespace Json.Facts
+{
+    public class OneOrMoreFacts
+    {
+        [Fact]
+        public void WhenMatchIsCalledAndTextEnteredIsNullOrEmptyReturnsNull()
+        {
+            var a = new OneOrMore(new Range('0', '9'));
+
+            Assert.False(a.Match(null).Succes());
+            Assert.False(a.Match("").Succes());
+        }
+
+        [Fact]
+        public void WhenMatchIsCalledAndPatternMatchesGivenTextReturnsTrueAndRemainingText()
+        {
+            var a = new OneOrMore(new Range('0', '9'));
+
+            Assert.True(a.Match("123").Succes() && a.Match("123").RemainingText().Equals(""));
+            Assert.True(a.Match("1a").Succes() && a.Match("1a").RemainingText().Equals("a"));
+        }
+
+        [Fact]
+        public void WhenmatchIsCalledAndGivenPatternDoesNotMatchTextEnteredReturnsFalseAndTextEntered()
+        {
+            var a = new OneOrMore(new Range('0', '9'));
+
+            Assert.False(a.Match("bc").Succes() && a.Match("bc").RemainingText().Equals("bc"));
+        }
+    }
+}
