a61308c69906d13f23431c5c18431bcf0edfa92c
SamiChira <99469882+SamiChira@users.noreply.github.com>
2022-06-06 09:33:52 +0300

    Added List.cs

---
+++ Json.Validator/List.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Json
+{
+    public class List : IPattern
+    {
+        readonly IPattern pattern;
+
+        public List(IPattern element, IPattern separator)
+        {
+            this.pattern = new Sequence(
+                    element,
+                    new Many(new Sequence(separator, element)));
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
