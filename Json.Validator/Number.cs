4d92e005b5888704a8f6e4f2f25950e7292a7a43
SamiChira <99469882+SamiChira@users.noreply.github.com>
2022-06-09 14:21:48 +0300

    Refractored Number.cs

--- Json.Validator/Number.cs
+++ Json.Validator/Number.cs
@@ -8,12 +8,11 @@ namespace Json
 
         public Number()
         {
-            var plusOrMinus = new Optional(new Choice(new Character('+'), new Character('-')));
+            var plusOrMinus = new Optional(new Any("+-"));
             var digits = new OneOrMore(new Range('0', '9'));
 
             var integer = new Sequence(plusOrMinus, new Choice(new Character('0'), digits));
             var fraction = new Optional(new Sequence(new Character('.'), digits));
-            var expoentTest = new Sequence(new Any("eE"), plusOrMinus, digits);
             var exponent = new Optional(new Sequence(new Any("eE"), plusOrMinus, digits));
 
             this.pattern = new Sequence(integer, fraction, exponent);
