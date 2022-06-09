9283e60c4a534f67fa52e1cf7e09714e30100251
SamiChira <99469882+SamiChira@users.noreply.github.com>
2022-06-09 13:33:56 +0300

    added Number.cs & NumberFacts.cs

---
+++ Json.Facts/NumberFacts.cs
@@ -0,0 +1,112 @@
+﻿using Xunit;
+
+namespace Json.Facts
+{
+    public class NumberFacts
+    {
+        [Fact]
+        public void WhenMatchIsCalledAndTextEnteredIsNullOrEmptyReturnsFalse()
+        {
+            var number = new Number();
+
+            Assert.False(number.Match("").Succes());
+            Assert.False(number.Match(null).Succes());
+        }
+
+        [Fact]
+        public void WhenMatchIsCalledAndTextEnteredIsAValidNegativeOrPositiveIntegerReturnsTrueAndNoRemainingText()
+        {
+            var number = new Number();
+
+            Assert.True(number.Match("1").Succes() && number.Match("1").RemainingText() == "");
+            Assert.True(number.Match("-0").Succes() && number.Match("-0").RemainingText() == "");
+            Assert.True(number.Match("-1").Succes() && number.Match("-1").RemainingText() == "");
+        }
+
+        [Fact]
+        public void WhenMatchIsCalledAndTextEnteredHasAValidIntegerReturnsTrueAndRemainingText()
+        {
+            var number = new Number();
+
+            Assert.True(number.Match("12a").Succes() && number.Match("a").RemainingText() == "a");
+        }
+
+        [Fact]
+        public void WhenMatchIsCalledAndTextEnteredIsNotAValidNumberReturnsFalseAndRemainingText()
+        {
+            var number = new Number();
+
+            Assert.False(number.Match("a").Succes() && number.Match("a").RemainingText() == "a");
+        }
+
+        [Fact]
+        public void WhenMatchIsCalledAndTextEnteredContainsANumberThatStartWhitZeroAndItHasOtherDigitsNotSeparedByDotReturnsTrueAndRemainindText()
+        {
+            var number = new Number();
+
+            Assert.True(number.Match("01").Succes() && number.Match("01").RemainingText() == "1");
+        }
+
+        [Fact]
+        public void WhenMatchIsCalledAndTextEnteredIsAValidFractionReturnsTrueAndNoRemainingText()
+        {
+            var number = new Number();
+
+            Assert.True(number.Match("1.1").Succes() && number.Match("1.1").RemainingText() == "");
+            Assert.True(number.Match("1.11111").Succes() && number.Match("1.11111").RemainingText() == "");
+        }
+
+        [Fact]
+        public void WhenMatchIsCalledAndTextEnteredContainsValidFractionReturnsTrueAndRemainingText()
+        {
+            var number = new Number();
+
+            Assert.True(number.Match("1.1.1").Succes() && number.Match("1.1.1").RemainingText() == ".1");
+        }
+
+        [Fact]
+        public void WhenMatchIsCalledAndTextEnteredIsAValidNumberAndHasExponentReturnsTrueAndNoRemainingText()
+        {
+            var number = new Number();
+
+            Assert.True(number.Match("1E1").Succes() && number.Match("1E1").RemainingText() == "");
+            Assert.True(number.Match("1e1").Succes() && number.Match("1e1").RemainingText() == "");
+        }
+
+        [Fact]
+        public void WhenMatchIsCalledAndTextEnteredIsAValidNumberAndHasExponentWithPlusOrMinusReturnsTrueAndNoRemainingText()
+        {
+            var number = new Number();
+
+            Assert.True(number.Match("1E+1").Succes() && number.Match("1E+1").RemainingText() == "");
+            Assert.True(number.Match("1e-1").Succes() && number.Match("1e-1").RemainingText() == "");
+        }
+
+        [Fact]
+        public void WhenMatchIsCalledAndTextEnteredExponentContainsOtherLettersReturnsTrueAndRemainingText()
+        {
+            var number = new Number();
+
+            Assert.True(number.Match("1e1a").Succes() && number.Match("1e1a").RemainingText() == "a");
+        }
+
+        [Fact]
+        public void WhenMatchIsCalledAndTextEnteredHasAInvalidExponentReturnsTrueAndNoRemainingText()
+        {
+            var number = new Number();
+
+            Assert.True(number.Match("1ea").Succes() && number.Match("1ea").RemainingText() == "ea");
+        }
+
+        [Fact]
+        public void WhenMatchIsCalledAndTextEnteredIsAValidNumberWithFractionAndExponentReturnsTrueAndNoRemainingText()
+        {
+            var number = new Number();
+
+            Assert.True(number.Match("1.1e1").Succes() && number.Match("1.1e1").RemainingText() == "");
+            Assert.True(number.Match("-1.1e1").Succes() && number.Match("-1.1e1").RemainingText() == "");
+        }
+
+
+    }
+}
---
+++ Json.Validator/Number.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Json
+{
+    public class Number : IPattern
+    {
+        readonly IPattern pattern;
+
+        public Number()
+        {
+            var plusOrMinus = new Optional(new Choice(new Character('+'), new Character('-')));
+            var digits = new OneOrMore(new Range('0', '9'));
+
+            var integer = new Sequence(plusOrMinus, new Choice(new Character('0'), digits));
+            var fraction = new Optional(new Sequence(new Character('.'), digits));
+            var expoentTest = new Sequence(new Any("eE"), plusOrMinus, digits);
+            var exponent = new Optional(new Sequence(new Any("eE"), plusOrMinus, digits));
+
+            this.pattern = new Sequence(integer, fraction, exponent);
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
