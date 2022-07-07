5989106ce2b7bfa5d1a7892e073964ca0a95e6d8
SamiChira <99469882+SamiChira@users.noreply.github.com>
2022-07-07 07:33:50 +0300

    Refractored Value.cs

--- Json.Validator/Value.cs
+++ Json.Validator/Value.cs
@@ -9,26 +9,23 @@ namespace Json
         public Value()
         {
             var value = new Choice(
-                        new String(),
-                        new Number(),
-                        new Text("true"),
-                        new Text("false"),
-                        new Text("null"));
+                         new String(),
+                         new Number(),
+                         new Text("true"),
+                         new Text("false"),
+                         new Text("null"));
 
-            var whiteSpace = new Any(" \n\r\t");
+            var whiteSpace = new Many(new Any(" \n\r\t"));
+
             var element = new Sequence(whiteSpace, value, whiteSpace);
-            var elements = new List(element, new Character(','));
+            var elements = new Sequence(whiteSpace, new List(element, new Character(',')), whiteSpace);
 
-            var array = new Choice(
-                        new Sequence(new Character('['), whiteSpace, new Character(']')),
-                        new Sequence(new Character('['), elements, new Character(']')));
+            var array = new Sequence(new Character('['), elements, new Character(']'));
 
             var member = new Sequence(whiteSpace, new String(), whiteSpace, new Character(':'), element);
-            var members = new List(member, new Character(','));
+            var members = new Sequence(whiteSpace, new List(member, new Character(',')), whiteSpace);
 
-            var obj = new Choice(
-                      new Sequence(new Character('{'), whiteSpace, new Character('}')),
-                      new Sequence(new Character('{'), members, new Character('}')));
+            var obj = new Sequence(new Character('{'), members, new Character('}'));
 
             value.Add(array);
             value.Add(obj);
