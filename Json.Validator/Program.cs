b52ad3b58beca20a04b0afe8567901ac54489201
SamiChira <99469882+SamiChira@users.noreply.github.com>
2022-07-18 15:23:19 +0300

    Refractored Progam.cs

--- Json.Validator/Program.cs
+++ Json.Validator/Program.cs
@@ -8,7 +8,7 @@ namespace Json
         {
             var value = new Value();
             var text = System.IO.File.ReadAllText(args[0]);
-            Console.WriteLine(value.Match(text).Succes() ? "Valid JSON format!" : "Invalid JSON format!");
+            Console.WriteLine(string.IsNullOrEmpty(value.Match(text).RemainingText()) ? "Valid JSON format!" : "Invalid JSON format!");
         }
     }
 }
