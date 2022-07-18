38205c4dca47a25d037e48acd1205c9d350388da
SamiChira <99469882+SamiChira@users.noreply.github.com>
2022-07-18 15:57:28 +0300

    Refractored Program.cs

--- Json.Validator/Program.cs
+++ Json.Validator/Program.cs
@@ -6,9 +6,16 @@ namespace Json
     {
         public static void Main(string[] args)
         {
-            var value = new Value();
-            var text = System.IO.File.ReadAllText(args[0]);
-            Console.WriteLine(string.IsNullOrEmpty(value.Match(text).RemainingText()) ? "Valid JSON format!" : "Invalid JSON format!");
+            if (args.Length > 0 && System.IO.File.Exists(args[0]))
+            {
+                var value = new Value();
+                var text = System.IO.File.ReadAllText(args[0]);
+                Console.WriteLine(string.IsNullOrEmpty(value.Match(text).RemainingText()) ? "Valid JSON format!" : "Invalid JSON format!");
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid path or check that a path is entered and refers to a valid text file! \nE.g: C:\\fileName.txt");
+            }
         }
     }
 }
