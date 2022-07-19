fc4f725a958f59333c12c4c3b7a05c912dff692f
SamiChira <99469882+SamiChira@users.noreply.github.com>
2022-07-19 16:00:57 +0300

    Refractored Program.cs & edited GlobalSupressions for naming equivalent (String.cs & String)

--- Json.Validator/Program.cs
+++ Json.Validator/Program.cs
@@ -6,16 +6,18 @@ namespace Json
     {
         public static void Main(string[] args)
         {
-            if (args.Length > 0 && System.IO.File.Exists(args[0]))
+            if (args.Length == 0 || !System.IO.File.Exists(args[0]))
             {
-                var value = new Value();
-                var text = System.IO.File.ReadAllText(args[0]);
-                Console.WriteLine(string.IsNullOrEmpty(value.Match(text).RemainingText()) ? "Valid JSON format!" : "Invalid JSON format!");
+                return;
+
+                // Please enter a valid path or check that a path is entered and refers to a valid text file! \nE.g: C:\\fileName.txt
             }
-            else
-            {
-                Console.WriteLine("Please enter a valid path or check that a path is entered and refers to a valid text file! \nE.g: C:\\fileName.txt");
-            }
+
+            var text = System.IO.File.ReadAllText(args[0]);
+            IMatch jsonValidator = new Value().Match(text);
+            Console.WriteLine(jsonValidator.Succes() && string.IsNullOrEmpty(jsonValidator.RemainingText()) ? "Valid JSON format!" : "Invalid JSON format!");
+
+            Console.ReadLine();
         }
     }
 }
